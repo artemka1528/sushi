fd3fd8e1f36f8c1fc05f4bd72ec0c5e266ab6aa5
Artem Puzyrev <puzyrev_artem@bk.ru>
2022-06-22 01:40:05 +0300

    add background in app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
+import "./App.css";
 
-import LeftSidebar from './Components/LeftSidebar/LeftSidebar';
+import LeftSidebar from "./Components/LeftSidebar/LeftSidebar";
 
-import Main from './Components/Main/Main';
-import Pizza from './Components/Pizza/Pizza';
-import Set from './Components/Set/Set';
-import Feedback from './Components/Feedback/Feedback';
+import Main from "./Components/Main/Main";
+import Pizza from "./Components/Pizza/Pizza";
+import Set from "./Components/Set/Set";
+import Feedback from "./Components/Feedback/Feedback";
 
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
 
-import Header from './Components/Header/Header';
+import Header from "./Components/Header/Header";
 
 import { Routes, Route, Link } from "react-router-dom";
 
-import { ThemeProvider } from '@mui/material';
+import { ThemeProvider } from "@mui/material";
 
-import { Theme } from './Theme';
-
-
+import { Theme } from "./Theme";
 
 function App() {
   console.log(Theme);
@@ -30,21 +28,25 @@ function App() {
             <Grid item xs={2}>
               <LeftSidebar />
             </Grid>
-            <Grid item xs={11}>
-              <Header  city='Бишкек' tel1='+996 705 188 955' tel2='+996 555 188 955'/>
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/pizza" element={<Pizza />} />
-              <Route path="/Feedback" element={<Feedback />} />
-              <Route path="/Set" element={<Set />} />
-            </Routes>
+            <Grid item xs={11} sx={{backgroundColor:'#F2F2F2'}}>
+              <Header
+                city="Бишкек"
+                tel1="+996 705 188 955"
+                tel2="+996 555 188 955"
+              />
+              <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/pizza" element={<Pizza />} />
+                <Route path="/Feedback" element={<Feedback />} />
+                <Route path="/Set" element={<Set />} />
+              </Routes>
             </Grid>
             <Grid item xs={3}>
               right-sidebar
             </Grid>
           </Grid>
         </Container>
-      </ThemeProvider> 
+      </ThemeProvider>
     </div>
   );
 }
