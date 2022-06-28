a6b14d1db00cdcbc6242a7a632a53cdf179a25ce
Artem Puzyrev <puzyrev_artem@bk.ru>
2022-06-28 16:54:16 +0300

    fix site set

--- src/Components/Card/CardItem.jsx
+++ src/Components/Card/CardItem.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { Divider } from "@mui/material";
+
+import { Divider, Typography, Button, CardMedia, CardContent, CardActions, Card } from "@mui/material";
+
+import imge from '../../img/product/1.png';
 
 function CardItem({name, pieces, weight, price}) {
   return (
     <>
-      <Card sx={{ maxWidth: 288}}>
+      <Card sx={{ maxWidth: 288, height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
         <CardMedia
           component="img"
           height="210"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={imge}
           alt="green iguana"
         />
         <CardContent>
